75baf1e5fdf69e122f9f5f2742cffd4a88b6311a
Jakub Podsadny <jakub.podsadny2000@gmail.com>
2020-09-15 16:28:22 +0200

    change folder's name

---
+++ docs/precache-manifest.24eeb0a0ca1e2f424a5b65ed2867b9f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf61f93ffff9acf1a6f809b2ef8892d",
+    "url": "/to_do_app_project/index.html"
+  },
+  {
+    "revision": "0cbbc15d87cba2196d54",
+    "url": "/to_do_app_project/static/css/main.26c9edb5.chunk.css"
+  },
+  {
+    "revision": "683d49bd29e9879486eb",
+    "url": "/to_do_app_project/static/js/2.a38ffbb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to_do_app_project/static/js/2.a38ffbb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cbbc15d87cba2196d54",
+    "url": "/to_do_app_project/static/js/main.676c181f.chunk.js"
+  },
+  {
+    "revision": "0b17782bf4e07518ac66",
+    "url": "/to_do_app_project/static/js/runtime-main.607e11cb.js"
+  }
+]);
